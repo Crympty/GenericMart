b7ae94caebff6d5c1d2eb3c02f7103ffe15c56bb
samxielaptop <crimsoncrympty@gmail.com>
2025-05-22 00:17:30 -0700

    process of making sidebar

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
-function runFunc() {
-    alert("welcome")
+document.getElementById("carticon").addEventListener("click", changeCartState);
+
+let CART_STATE = "CLOSED";
+function changeCartState() {
+
+    if (CART_STATE == "CLOSED") {
+        gsap.to("#sidebar", {width: "200px", duration: 0.7, ease:"power2.out"});
+        CART_STATE = "OPEN";
+    } else {
+        gsap.to("#sidebar", {width: "0", duration: 0.7, ease:"power2.in"});
+        CART_STATE = "CLOSED";
+    }
 }
