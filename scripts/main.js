e10ec340d716c3574663c808ad740e3b71ab6bd4
samxielaptop <crimsoncrympty@gmail.com>
2025-05-25 15:07:15 -0700

    pissmitt

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,20 @@
-document.getElementById("carticon").addEventListener("click", changeCartState);
 
-let CART_STATE = "CLOSED";
-function changeCartState() {
 
-    if (CART_STATE == "CLOSED") {
-        gsap.to("#sidebar", {width: "200px", duration: 0.7, ease:"power2.out"});
-        CART_STATE = "OPEN";
+let sidebarslid = false;
+let sidebarTl = gsap.timeline(paused(true));
+
+//retract
+sidebarTl.to("#sidebar", {width: "0", duration: 0.7, ease:"power2.in"});
+
+//extend
+sidebarTl.to("#sidebar", {width: "200px", duration: 0.7, ease:"power2.out"});
+sidebarTl.to("#sidebar > *", );
+
+
+document.getElementById("carticon").addEventListener("click", () => {
+    if (!sidebarslid) {
+        sidebarTl.play();
     } else {
-        gsap.to("#sidebar", {width: "0", duration: 0.7, ease:"power2.in"});
-        CART_STATE = "CLOSED";
+        sidebarTl.reverse()
     }
-}
+});
