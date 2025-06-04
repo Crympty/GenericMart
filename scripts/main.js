bb9d5c326aa2dc64f66fd57a95d973fea9efd854
samxiepc <crimsoncrympty@gmail.com>
2025-06-04 02:55:07 -0700

    commit for restauraunt

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 
 
 let sidebarTl = gsap.timeline({paused: true});
+let sidebarslid = false;
+let itemCount = 1;
 
 sidebarTl.fromTo("#sidebar", { x: "-100%" }, { x: "0%", duration: 0.7, ease: "power2.inOut" }, 0);
 
-let sidebarslid = false;
 document.getElementById("carticon").addEventListener("click", () => {
+    const sidebar = document.getElementById("sidebar");
     if (!sidebarslid) {
         sidebarTl.play();
+        sidebar.removeAttribute("inert");
     } else {
         sidebarTl.reverse();
     }
@@ -24,8 +27,54 @@ document.addEventListener("click", function (event) {
     }
     if (!sidebar.contains(event.target) && !carticon.contains(event.target)) {
         sidebarTl.reverse();
+        siderbar.setAttribute("inert", "");
         sidebarslid = !sidebarslid;
     }
 });
 
 //TODO: add a items count updater for sidebar top
+
+document.getElementById("sidebar-container").addEventListener("click", function(e) {
+    if (e.target.classList.contains("item-trash")) {
+        const itemG = e.target.closest(".item-group")
+        //TODO: impl change cart total
+
+        itemG.remove();
+    }
+});
+
+document.getElementById("listing-container").addEventListener("click", function (e) {
+    console.log(e);
+    if (e.target.classList.contains("add-cart-btn")) {
+        handleCartAdd();
+    }
+    if (e.target.classList.contains("l-q-container")) {
+        if (e.target.classList.contains("listing-add")) {
+            handleQuantityChange(true, e);
+        }
+        if (e.target.classList.contains("listing-minus")) {
+            handleQuantityChange(false, e);
+        }
+    }
+})
+
+
+function handleCartAdd(event) {
+
+}
+
+function addItem() {
+    const template = document.getElementById("cart-item-template");
+    const clone = template.content.cloneNode(true);
+    let newItem = clone.querySelector(".item-group");
+
+    newItem.id = `cart-item${itemCount}`
+    itemCount++;
+
+    console.log(clone);
+    document.getElementById("sidebar-container").appendChild(clone);
+}
+
+function handleQuantityChange(isIncrease, event) {
+
+}
